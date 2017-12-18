819db561d1cb7e059e2f4e92ec04d871b8dd109e
Thiago Andrade <thiagoblandrade@MacBook-Air-de-Thiago.local>
2017-12-17 23:21:48 -0600

    Add files to Github

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Thiago Andrade on 12/17/17.
+//  Copyright © 2017 Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var calcDisplay: UILabel!
+    let buttom0: Int = 0
+    let buttom1: Int = 1
+    let buttom2: Int = 2
+    let buttom3: Int = 3
+    let buttom4: Int = 4
+    let buttom5: Int = 5
+    let buttom6: Int = 6
+    let buttom7: Int = 7
+    let buttom8: Int = 8
+    let buttom9: Int = 9
+    var number1: Double = 0.00
+    var number2: Double = 0.00
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
